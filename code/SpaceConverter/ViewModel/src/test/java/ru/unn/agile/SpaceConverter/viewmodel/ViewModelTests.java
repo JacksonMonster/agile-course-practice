aad16ea43c6ae3eb67d7dd5da025375049423da0
Ryabinkin Evgeni <JacksonMonster@users.noreply.github.com>
2017-03-04 02:03:18 +0300

    fixed viewModelTests ver2

--- code/SpaceConverter/ViewModel/src/test/java/ru/unn/agile/SpaceConverter/viewmodel/ViewModelTests.java
+++ code/SpaceConverter/ViewModel/src/test/java/ru/unn/agile/SpaceConverter/viewmodel/ViewModelTests.java
@@ -104,4 +104,85 @@ public class ViewModelTests {
 
         assertEquals("1.0E-6", viewModel.resultValueProperty().get());
     }
+    @Test
+    public void consToHectareMulHasCorrectResult() {
+        viewModel.sqMeterTextBoxProperty().set("1");
+        viewModel.constantProperty().set(Cons.TO_HECTARE_MULTIPLIER);
+
+        viewModel.convert();
+
+        assertEquals("1.0E-4", viewModel.resultValueProperty().get());
+    }
+    @Test
+    public void consToArMulHasCorrectResult() {
+        viewModel.sqMeterTextBoxProperty().set("1");
+        viewModel.constantProperty().set(Cons.TO_AR_MULTIPLIER);
+
+        viewModel.convert();
+
+        assertEquals("0.01", viewModel.resultValueProperty().get());
+    }
+    @Test
+    public void consToSantimeterMulHasCorrectResult() {
+        viewModel.sqMeterTextBoxProperty().set("1");
+        viewModel.constantProperty().set(Cons.TO_SQR_SANTIMETER_MULTIPLIER);
+
+        viewModel.convert();
+
+        assertEquals("10000.0", viewModel.resultValueProperty().get());
+    }
+    @Test
+    public void consToMillimeterMulHasCorrectResult() {
+        viewModel.sqMeterTextBoxProperty().set("1");
+        viewModel.constantProperty().set(Cons.TO_SQR_MILLIMETER_MULTIPLIER);
+
+        viewModel.convert();
+
+        assertEquals("1000000.0", viewModel.resultValueProperty().get());
+    }
+    @Test
+    public void consToYardMulHasCorrectResult() {
+        viewModel.sqMeterTextBoxProperty().set("1");
+        viewModel.constantProperty().set(Cons.TO_SQR_YARD_MULTIPLIER);
+
+        viewModel.convert();
+
+        assertEquals("1.196", viewModel.resultValueProperty().get());
+    }
+    @Test
+    public void consToFootMulHasCorrectResult() {
+        viewModel.sqMeterTextBoxProperty().set("1");
+        viewModel.constantProperty().set(Cons.TO_SQR_FOOT_MULTIPLIER);
+
+        viewModel.convert();
+
+        assertEquals("10.76", viewModel.resultValueProperty().get());
+    }
+    @Test
+    public void consToInchMulHasCorrectResult() {
+        viewModel.sqMeterTextBoxProperty().set("1");
+        viewModel.constantProperty().set(Cons.TO_SQR_INCH_MULTIPLIER);
+
+        viewModel.convert();
+
+        assertEquals("1550.0", viewModel.resultValueProperty().get());
+    }
+    @Test
+    public void consToAcreMulHasCorrectResult() {
+        viewModel.sqMeterTextBoxProperty().set("1");
+        viewModel.constantProperty().set(Cons.TO_SQR_ACRE_MULTIPLIER);
+
+        viewModel.convert();
+
+        assertEquals("2.471E-4", viewModel.resultValueProperty().get());
+    }
+    @Test
+    public void consToMileMulHasCorrectResult() {
+        viewModel.sqMeterTextBoxProperty().set("1");
+        viewModel.constantProperty().set(Cons.TO_SQR_MILE_MULTIPLIER);
+
+        viewModel.convert();
+
+        assertEquals("3.861E-7", viewModel.resultValueProperty().get());
+    }
 }
